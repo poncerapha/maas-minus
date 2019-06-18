39ed98f262a57704631986efcf228806327fdec4
Raphael Ponce <raphael.fleury@b2wdigital.com>
2019-06-18 16:26:34 -0300

    Implementação minus

--- maas/app.py
+++ maas/app.py
@@ -1,9 +1,11 @@
 from aiohttp import web
 from asyncworker import App, RouteTypes
 
-app = App("", "", "", 1)
 
+app = App("", "", "", 1)
 
 @app.route(["/"], type=RouteTypes.HTTP, methods=["POST"])
 async def operation(request: web.Request):
-    return web.json_response({})
+    data = await request.json()
+    return {"result": data["left"] - data["right"]}
+        
