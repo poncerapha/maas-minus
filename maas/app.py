82fab954125125dc0ef1871a4845f7128dc47189
Raphael Ponce <raphael.fleury@b2wdigital.com>
2019-06-18 16:38:43 -0300

    função de subtracao

--- maas/app.py
+++ maas/app.py
@@ -1,11 +1,10 @@
 from aiohttp import web
 from asyncworker import App, RouteTypes
 
-
 app = App("", "", "", 1)
+
 
 @app.route(["/"], type=RouteTypes.HTTP, methods=["POST"])
 async def operation(request: web.Request):
     data = await request.json()
     return {"result": data["left"] - data["right"]}
-        
